58778c5c465e2dae8f7ec2391440219f34ed9018
DanHosman <francis_coder@outlook.com>
2025-10-29 14:31:17 +0100

    updated hello test

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 
 # simple repository test
 
-hello = "Hello Dude"
+class Hello:
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
+
+    def __str__(self):
+        return f"Hi my name is {self.name}, {self.message}"
+
+hello = Hello("Franz", "I'm learning Python")
 
 print(hello)
